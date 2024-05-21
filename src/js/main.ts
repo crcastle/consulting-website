cc2ed0bcac422064cc07a9b646c9d80c06664dad
Chris Castle <github@crc.io>
2024-05-20 18:59:58 -0700

    Get Contact form working

--- src/js/main.ts
+++ src/js/main.ts
@@ -24,3 +24,50 @@ import '../styles/style.css';
 // `
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+const contactPath = '/consulting-website/contact';
+
+const contactBtn = document.querySelector<HTMLButtonElement>('#contact-btn');
+if (contactBtn) {
+  contactBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (window.location.pathname !== contactPath) {
+      showContactForm();
+      history.pushState({ url: contactPath }, '', contactPath)
+    }
+
+    e.stopPropagation();
+  }, false);
+}
+
+window.addEventListener('popstate', (e) => {
+  toggle(e.state);
+});
+
+function showContactForm() {
+  const contactForm = document.getElementById('contact-form');
+  const mainContent = document.getElementById('main-content');
+
+  if (contactForm && mainContent) {
+    contactForm.classList.remove('inactive');
+    mainContent.classList.add('inactive');
+  }
+}
+
+function hideContactForm() {
+  const contactForm = document.getElementById('contact-form');
+  const mainContent = document.getElementById('main-content');
+
+  if (contactForm && mainContent) {
+    contactForm.classList.add('inactive');
+    mainContent.classList.remove('inactive');
+  }
+}
+
+function toggle(state: PopStateEvent["state"]) {
+  if (state && state.url === contactPath) {
+    showContactForm();
+  } else {
+    hideContactForm();
+  }
+}
